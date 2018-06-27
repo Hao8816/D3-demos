dc1637737bb28f453b3da829cd41a2ad08adc99b
Hao8816 <chenhao8816+8816@gmail.com>
2018-06-27 23:08:28 +0800

    修改

--- app/main.js
+++ app/main.js
@@ -1,4 +1,19 @@
-const greeter = require('./Greeter.js');
-const style = require('./main.css')
-console.log(greeter);
-document.querySelector("#root").appendChild(greeter());
+require('angular');
+require('angular-route');
+require('./main.css');
+var app = angular.module('web', ['ngRoute']);
+
+// 配置路由
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+        when('/index/', {
+            templateUrl: './app/views/app.html',
+            controller: 'indexPageController'
+        }).
+        otherwise({redirectTo: '/index/'});
+
+}]);
+app.controller("indexPageController",function($scope){
+    $scope.names = [1,2,3,4,5,6,7,8,9];
+});
+
